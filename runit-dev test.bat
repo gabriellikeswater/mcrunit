524039eae3b98a0a54e2175c78f87acd994a55a6
Gabriel Likes Salad <90223564+gabriellikeswater@users.noreply.github.com>
2023-04-10 19:57:56 +0100

    new test version of the program (temporary)
    Big BIG changes.

---
+++ runit-dev test.bat
@@ -0,0 +1,261 @@
+
+::FOR DEVS: COMMENTS HAVE A "::".
+::COMMENTS TELL YOU WHAT YOU CAN CHANGE.
+
+@echo off
+title ARE YOU DREAMING?
+
+set "filename=%~f0"
+
+:mainarea
+
+cls
+
+echo                    are you dreaming?
+echo    ______     __  __     __   __        __     ______  
+echo   /\  == \   /\ \/\ \   /\ "-.\ \      /\ \   /\__  _\ 
+echo   \ \  __-   \ \ \_\ \  \ \ \-.  \     \ \ \  \/_/\ \/ 
+echo    \ \_\ \_\  \ \_____\  \ \_\\"\_\     \ \_\    \ \_\ 
+echo     \/_/ /_/   \/_____/   \/_/ \/_/      \/_/     \/_/ 
+echo.
+echo  - Type "configarea" to configure the server to your liking.                                                   
+echo  - Type "runarea" to run this server.
+echo  - Type "mainarea" to go back to this area.
+echo  - While running, type "stop" to stop the server.
+echo  - While running, type "help" for a list of all commands.
+echo.
+echo  -----------------------Notes(below)---------------------
+echo.
+echo  - After executing the "runall" command, two windows will
+echo    show up. DON'T CLOSE THEM!
+echo  - You can't close both windows with the "stop" command
+echo    so you have to manually close them :D
+echo  - Anything typed in that's not a command in the program
+echo    will close the program.
+echo  - Remember, all of this ONLY works for WINDOWS.
+echo.
+
+set /p option1= [%username%@runit]~ 
+if %option1% == configarea goto configarea
+if %option1% == runarea goto runarea
+if %option1% == mainarea goto mainarea
+if %option1% == runall goto runall
+if %option1% == run goto run
+if %option1% == runproxy goto runproxy ::OPTIONAL
+if %option1% == runbot goto runbot ::OPTIONAL
+
+goto
+
+pause
+
+:configarea
+cls
+
+echo.
+echo      TYPE "mainarea" TO GO BACK TO THE MAIN AREA.
+echo.
+echo  - Type "changeram" to change the ammount of ram you
+echo    want to allocate to your server
+echo  - Type "changeapi" to change the server api to run,
+echo    for example, paper.jar
+echo.
+
+set /p option1= [%username%@runit]~ 
+if %option1% == changeram goto changeram
+if %option1% == changeapi goto changeapi
+if %option1% == configarea goto configarea
+if %option1% == mainarea goto mainarea
+if %option1% == runarea goto runarea
+if %option1% == runall goto runall
+if %option1% == run goto run
+if %option1% == runproxy goto runproxy ::OPTIONAL
+if %option1% == runbot goto runbot ::OPTIONAL
+
+goto
+
+pause
+
+:changeram
+
+echo.
+
+set /p input=Type "Xms2G"(Min Ram usable):
+set /p replacement=Type it again but with your number of RAM:
+
+powershell -Command "(Get-Content '%filename%') | Foreach-Object {$_ -replace '%input%', '%replacement%'} | Set-Content '%filename%'
+
+set /p input2=Type "Xmx3G"(Max Ram usable):
+set /p replacement2=Type it again but with your number of RAM:
+
+powershell -Command "(Get-Content '%filename%') | Foreach-Object {$_ -replace '%input2%', '%replacement2%'} | Set-Content '%filename%'
+echo Done.
+echo.
+
+set /p option1= [%username%@runit]~ 
+if %option1% == changeram goto changeram
+if %option1% == changeapi goto changeapi
+if %option1% == configarea goto configarea
+if %option1% == mainarea goto mainarea
+if %option1% == runarea goto runarea
+if %option1% == runall goto runall
+if %option1% == run goto run
+if %option1% == runproxy goto runproxy ::OPTIONAL
+if %option1% == runbot goto runbot ::OPTIONAL
+
+goto
+
+pause
+
+:changeapi
+
+echo.
+
+set /p input3=Type "arclight":
+set /p replacement3=Now, type your api:
+
+powershell -Command "(Get-Content '%filename%') | Foreach-Object {$_ -replace '%input3%', '%replacement3%'} | Set-Content '%filename%'
+echo Done.
+echo.
+
+set /p option1= [%username%@runit]~ 
+if %option1% == changeram goto changeram
+if %option1% == changeapi goto changeapi
+if %option1% == configarea goto configarea
+if %option1% == mainarea goto mainarea
+if %option1% == runarea goto runarea
+if %option1% == runall goto runall
+if %option1% == run goto run
+if %option1% == runproxy goto runproxy ::OPTIONAL
+if %option1% == runbot goto runbot ::OPTIONAL
+
+goto
+
+pause
+
+:runarea
+cls
+
+echo.
+echo      TYPE "mainarea" TO GO BACK TO THE MAIN AREA.
+echo.
+echo  - To run all the stuff needed for the server, Type "runall"
+echo    (If you don't have a discord bot or playit proxy
+echo    you might get an error, it's OK. The server will
+echo    still work :D )
+echo  - To run only the local server, Type "run"
+echo  - To run only the playit proxy, Type "runproxy" ::OPTIONAL
+echo  - To run only the discord bot, Type "runbot" ::OPTIONAL
+echo.
+
+set /p option1= [%username%@runit]~ 
+if %option1% == configarea goto configarea
+if %option1% == runarea goto runarea
+if %option1% == mainarea goto mainarea
+if %option1% == runall goto runall
+if %option1% == run goto run
+if %option1% == runproxy goto runproxy
+if %option1% == runbot goto runbot
+
+goto
+
+pause
+
+:runall
+
+cls
+
+echo.
+echo   Please be patient, this may take a bit!
+echo.
+
+start "..\Python\Python311\python.exe" "dbot.py" ::OPTIONAL
+
+start playit.exe ::OPTIONAL
+
+java -Xmx3G -Xms2G -jar arclight.jar nogui ::CHANGEABLE: -Xmx3G & arclight.jar
+echo.
+
+echo   TYPE "mainarea" TO GO BACK TO THE MAIN AREA.
+echo.
+
+set /p option1= [%username%@runit]~ 
+if %option1% == configarea goto configarea
+if %option1% == mainarea goto mainarea
+if %option1% == runarea goto runarea
+if %option1% == runall goto runall
+if %option1% == run goto run
+if %option1% == runproxy goto runproxy ::OPTIONAL
+if %option1% == runbot goto runbot ::OPTIONAL
+
+goto
+
+pause
+
+:run
+
+cls
+
+echo.
+echo   Please be patient, this may take a bit!
+echo.
+
+java -Xmx3G -Xms2G -jar arclight.jar nogui ::CHANGEABLE: -Xmx3G & arclight.jar
+echo.
+
+echo   TYPE "mainarea" TO GO BACK TO THE MAIN AREA.
+echo.
+
+set /p option1= [%username%@runit]~ 
+if %option1% == configarea goto configarea
+if %option1% == mainarea goto mainarea
+if %option1% == runarea goto runarea
+if %option1% == runall goto runall
+if %option1% == run goto run
+if %option1% == runproxy goto runproxy ::OPTIONAL
+if %option1% == runbot goto runbot ::OPTIONAL
+
+goto
+
+pause
+
+:runproxy ::OPTIONAL
+
+start playit.exe ::OPTIONAL
+
+echo   TYPE "mainarea" TO GO BACK TO THE MAIN AREA. ::OPTIONAL
+echo. ::OPTIONAL
+
+set /p option1= [%username%@runit]~ ::OPTIONAL
+if %option1% == configarea goto configarea ::OPTIONAL
+if %option1% == mainarea goto mainarea ::OPTIONAL
+if %option1% == runarea goto runarea ::OPTIONAL
+if %option1% == runall goto runall ::OPTIONAL
+if %option1% == run goto run ::OPTIONAL
+if %option1% == runproxy goto runproxy ::OPTIONAL
+if %option1% == runbot goto runbot ::OPTIONAL
+
+goto ::OPTIONAL
+
+pause ::OPTIONAL
+
+:runbot ::OPTIONAL
+
+echo. ::OPTIONAL
+
+start "..\Python\Python311\python.exe" "dbot.py" ::OPTIONAL
+
+echo   TYPE "mainarea" TO GO BACK TO THE MAIN AREA. ::OPTIONAL
+echo. ::OPTIONAL
+
+set /p option1= [%username%]~ ::OPTIONAL
+if %option1% == configarea goto configarea ::OPTIONAL
+if %option1% == mainarea goto mainarea ::OPTIONAL
+if %option1% == runarea goto runarea ::OPTIONAL
+if %option1% == runall goto runall ::OPTIONAL
+if %option1% == run goto run ::OPTIONAL
+if %option1% == runproxy goto runproxy ::OPTIONAL
+if %option1% == runbot goto runbot ::OPTIONAL
+
+goto
+
+pause
